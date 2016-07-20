1ce6e1bdfc70c85bd4fc8baf00da724b8a2d7ec3
Ariel Tay <shihqian@gmail.com>
2016-07-20 14:24:21 +0800

    include price in the directory

--- js/directory.js
+++ js/directory.js
@@ -1,9 +1,15 @@
 /* global $  */
+var count = 0
 $(function () {
   $('.dropdown-Categories').on('click', 'li', function (event) {
     var categories = $(this).text()
     console.log(categories)
     getCategories(categories)
+  })
+  $('.dropdown-Price').on('click', 'li', function (event) {
+    var price = $(this).text()
+    console.log(price)
+    getPrice(price)
   })
 })
 
@@ -15,7 +21,21 @@ function getCategories (categories) {
   .done(function (data) {
     console.log(data.makan)
     data.makan.forEach(function (datum) {
-      // $('#main').append('<div class="col-xs-6 col-md-3"> <div class="thumbnail color">' + datum.name + '-' + datum.categories + '</div></div>').css('height', '300px')
+      $('#main').append('<div class="panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div class=" bigger-font"><strong>Place: </strong>' + datum.name + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p></div></div></div>')
+    })
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log(errorThrown)
+  })
+}
+
+function getPrice (price) {
+  $('#main').empty()
+  $('#indexText').empty()
+  $('#indexText').append('<h4> Here are the Categories inside: ' + price + '</h4>')
+  $.get('https://tolomakan.herokuapp.com/price/' + price)
+  .done(function (data) {
+    console.log(data.makan)
+    data.makan.forEach(function (datum) {
       $('#main').append('<div class="panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div class=" bigger-font"><strong>Place: </strong>' + datum.name + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p></div></div></div>')
     })
     // console.log(data)
