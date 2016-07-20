f1ae486800d4dd6a34e75b70aa6a1ad885eccfab
Ariel Tay <shihqian@gmail.com>
2016-07-21 00:46:15 +0800

    testing at 12:46

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 /* global $ pos google*/
+var pinLatitude
+var pinLongitude
 $(function () {
   $('#tolo').click(function () {
     fetchTolo()
@@ -33,22 +35,22 @@ function fetchMakan () {
     console.log(errorThrown)
   })
 }
-function fetchTolo () {
-  $('#main').empty()
-  $('#indexText').empty()
-  $('#indexText').append('<h4> Here are the places you should makan today!</h4>')
-  var data = 'lng=' + pos.lng + '&' + 'lat=' + pos.lat
-  $.get('https://tolomakan.herokuapp.com/randomFive?' + data)
-  .done(function (data) {
-    data.forEach(function (datum) {
-      // $('#main').append('<div class="col-xs-6 col-md-3"> <div class="thumbnail color"> <b>Place: </b>' + datum.name + '<br/>' + '<b>Address:</b> ' + datum.address + '<b>Categories:</b> ' + datum.categories + '</div></div>')
-      // $('#main').append('<h4 class="media-heading"> <b>Place: </b>' + datum.name + '</h4>'
-      $('#main').append('<div class="makanOption panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div id="value" class=" bigger-font"><strong>Place: </strong>' + '<span class=datumName>' + datum.name + '</span>' + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p><p class="latitude">' + datum.latitude + '</p><p class="longitude">' + datum.longitude + '</p></div></div></div>')
-    })
-    // console.log(data)
-  }).fail(function (jqXHR, textStatus, errorThrown) {
-    console.log(errorThrown)
-  })
+function fetchTolo() {
+    $('#main').empty()
+    $('#indexText').empty()
+    $('#indexText').append('<h3> Here are the places you should makan today!</h3>')
+    var data = 'lng=' + pos.lng + '&' + 'lat=' + pos.lat
+    $.get('https://tolomakan.herokuapp.com/randomFive?' + data)
+        .done(function(data) {
+            data.forEach(function(datum) {
+                    // $('#main').append('<div class="col-xs-6 col-md-3"> <div class="thumbnail color"> <b>Place: </b>' + datum.name + '<br/>' + '<b>Address:</b> ' + datum.address + '<b>Categories:</b> ' + datum.categories + '</div></div>')
+                    // $('#main').append('<h4 class="media-heading"> <b>Place: </b>' + datum.name + '</h4>'
+                    $('#main').append('<div class="makanOption panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div id="value" class=" bigger-font"><strong>Place: </strong>' + '<span class=datumName>' + datum.name + '</span>' + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p><p class="latitude">' + datum.latitude + '</p><p class="longitude">' + datum.longitude + '</p></div></div></div>')
+                })
+                // console.log(data)
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown)
+        })
 }
 
 function fetchAddress(){
