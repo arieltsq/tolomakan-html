14d9dce110c75696b7e2aa835973529c40b711da
Ariel Tay <shihqian@gmail.com>
2016-07-21 00:43:56 +0800

    testing at 12.43am

--- js/index.js
+++ js/index.js
@@ -43,7 +43,7 @@ function fetchTolo () {
     data.forEach(function (datum) {
       // $('#main').append('<div class="col-xs-6 col-md-3"> <div class="thumbnail color"> <b>Place: </b>' + datum.name + '<br/>' + '<b>Address:</b> ' + datum.address + '<b>Categories:</b> ' + datum.categories + '</div></div>')
       // $('#main').append('<h4 class="media-heading"> <b>Place: </b>' + datum.name + '</h4>'
-      $('#main').append('<div class="makanOption panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div class=" bigger-font"><strong>Place: </strong>' + datum.name + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p></div></div></div>')
+      $('#main').append('<div class="makanOption panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div id="value" class=" bigger-font"><strong>Place: </strong>' + '<span class=datumName>' + datum.name + '</span>' + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p><p class="latitude">' + datum.latitude + '</p><p class="longitude">' + datum.longitude + '</p></div></div></div>')
     })
     // console.log(data)
   }).fail(function (jqXHR, textStatus, errorThrown) {
