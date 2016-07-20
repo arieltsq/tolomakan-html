57ee248abd8d14ad7894c3468b631156d229dcae
Ariel Tay <shihqian@gmail.com>
2016-07-20 16:37:10 +0800

    testing index.js map

--- js/index.js
+++ js/index.js
@@ -68,19 +68,19 @@ function geocodeLatLng(geocoder, map, infowindow) {
    var latlng = {lat: parseFloat(pos.lat), lng: parseFloat(pos.lng)}
   geocoder.geocode({'location': latlng} , function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
-      if (results[1]) {
+      // if (results[1]) {
         // map.setZoom(11)
         // var marker = new google.maps.Marker({
         //   position: latlng,
         //   map: map
         // });
-        $('#currentLocation').append(results[1].formatted_address)
+        $('#indexText').append(results.formatted_address)
         // infowindow.setContent(results[1].formatted_address);
         // infowindow.open(map, marker);
-      } else {
-        window.alert('No results found');
-      }
-console.log("outside of status ok")
+      // } else {
+      //   window.alert('No results found');
+      // }
+console.log("inside of status ok")
    } else {
       // window.alert('Geocoder failed due to: ' + status);
     }
