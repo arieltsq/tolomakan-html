87365679d26b6f4793ea81afc6aedb51606c1981
Ariel Tay <shihqian@gmail.com>
2016-07-21 01:24:33 +0800

    edited out the logic that didn't work

--- js/map.js
+++ js/map.js
@@ -1,10 +1,4 @@
-$(document).on('click', '#getMore', function () {
-  fetchMapTolo()
-  fetchMapMakan()
-})
 
-var pinLatitude
-var pinLongitude
 
 function initMap1() {
   var directionsDisplay = new google.maps.DirectionsRenderer;
@@ -38,29 +32,4 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
     }
   })
 }
-
-function fetchMapTolo () {
-  $('#main').empty()
-  console.log("testing the map js logic")
-  //  1.278668, 103.841098
-  // var data = 'lng=' + pos.lng + '&' + 'lat=' + pos.lat
-  var data = 'lng=' + 103.841098 + '&' + 'lat=' + 1.278668
-  $.get('https://tolomakan.herokuapp.com/randomFive?' + data)
-  .done(function (data) {
-    data.forEach(function (datum) {
-  $('#main').append('<div class="makanOption panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div id="value" class=" bigger-font"><strong>Place: </strong>' + '<span class=datumName>' + datum.name + '</span>' + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p><p class="latitude hide">' + datum.latitude + '</p><p class="longitude hide">' + datum.longitude + '</p></div></div></div>')
-    })
-    // console.log(data)
-  }).fail(function (jqXHR, textStatus, errorThrown) {
-    console.log(errorThrown)
-  })
-}
-
-function fetchMapMakan () {
-  pinLatitude = $('.latitude').first().text()
-  pinLongitude = $('.longitude').first().text()
-  console.log(pinLatitude)
-  console.log(pinLongitude)
-  $('#main').empty()
-  window.location.href = './map.html?pinLat=' + pinLatitude + '&pinLng=' + pinLongitude + '&posLng=' + pos.lng + '&posLat=' + pos.lat
-}
+s
