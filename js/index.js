19fae04242ccf7c8c013c557bb75f3601dec0b9f
Ariel Tay <shihqian@gmail.com>
2016-07-21 11:49:20 +0800

    fixed linter errors

--- js/index.js
+++ js/index.js
@@ -1,67 +1,62 @@
-/* global $  */
+/* global $ pos */
 var pinLatitude
 var pinLongitude
-$(function() {
-    $('#tolo').click(function() {
-        fetchTolo()
-    })
-    $('#random').click(function() {
-        fetchMakan()
-    })
+$(function () {
+  $('#tolo').click(function () {
+    fetchTolo()
+  })
+  $('#random').click(function () {
+    fetchMakan()
+  })
 })
 
 $(document).on('click', '.makanOption', function () {
-    var makanId = $(this).attr('id')
-    console.log(makanId)
-    console.log('click')
-    fetchMap(makanId)
+  var makanId = $(this).attr('id')
+  console.log(makanId)
+  console.log('click')
+  fetchMap(makanId)
 })
 
-
-function fetchMakan() {
-    $('#main').empty()
-    $('#indexText').empty()
-    $('#indexText').append('<h3>The MAKAN Master has spoke: </h3>')
-    $.get('https://tolomakan.herokuapp.com/makans')
-        .done(function(data) {
-            data.forEach(function(datum) {
-                    // $('#main').append('<div class="col-xs-6 col-md-3"> <div class="thumbnail color">' + datum.name + '-' + datum.categories + '</div></div>').css('height', '300px')
-                    $('#main').append('<div class="panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div class=" bigger-font"><strong>Place: </strong>' + datum.name + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p>' + '<button class="edit">Edit</button></div></div></div>')
-                })
-                // console.log(data)
-        }).fail(function(jqXHR, textStatus, errorThrown) {
-            console.log(errorThrown)
-        })
+function fetchMakan () {
+  $('#main').empty()
+  $('#indexText').empty()
+  $('#indexText').append('<h3>The MAKAN Master has spoke: </h3>')
+  $.get('https://tolomakan.herokuapp.com/makans')
+  .done(function (data) {
+    data.forEach(function (datum) {
+      // $('#main').append('<div class="col-xs-6 col-md-3"> <div class="thumbnail color">' + datum.name + '-' + datum.categories + '</div></div>').css('height', '300px')
+      $('#main').append('<div class="panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div class=" bigger-font"><strong>Place: </strong>' + datum.name + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p>' + '<button class="edit">Edit</button></div></div></div>')
+    })
+    // console.log(data)
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log(errorThrown)
+  })
 }
 
-
-
-function fetchTolo() {
-    $('#main').empty()
-    $('#indexText').empty()
-    $('#indexText').append('<h3> Here are the places you should makan today!</h3>')
-    var data = 'lng=' + pos.lng + '&' + 'lat=' + pos.lat
-    $.get('https://tolomakan.herokuapp.com/randomFive?' + data)
-        .done(function(data) {
-            data.forEach(function(datum) {
-                    // $('#main').append('<div class="col-xs-6 col-md-3"> <div class="thumbnail color"> <b>Place: </b>' + datum.name + '<br/>' + '<b>Address:</b> ' + datum.address + '<b>Categories:</b> ' + datum.categories + '</div></div>')
-                    // $('#main').append('<h4 class="media-heading"> <b>Place: </b>' + datum.name + '</h4>'
-                    $('#main').append('<div id="'+data.indexOf(datum)+'" class="makanOption panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div id="value" class=" bigger-font"><strong>Place: </strong>' + '<span class=datumName>' + datum.name + '</span>' + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p><p class="latitude">' + datum.latitude + '</p><p class="longitude">' + datum.longitude + '</p></div></div></div>')
-                })
-                // console.log(data)
-        }).fail(function(jqXHR, textStatus, errorThrown) {
-            console.log(errorThrown)
-        })
+function fetchTolo () {
+  $('#main').empty()
+  $('#indexText').empty()
+  $('#indexText').append('<h3> Here are the places you should makan today!</h3>')
+  var data = 'lng=' + pos.lng + '&' + 'lat=' + pos.lat
+  $.get('https://tolomakan.herokuapp.com/randomFive?' + data)
+  .done(function (data) {
+    data.forEach(function (datum) {
+      $('#main').append('<div id="' + data.indexOf(datum) + '" class="makanOption panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div id="value" class=" bigger-font"><strong>Place: </strong>' + '<span class=datumName>' + datum.name + '</span>' + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p><p class="latitude">' + datum.latitude + '</p><p class="longitude">' + datum.longitude + '</p></div></div></div>')
+    })
+    // console.log(data)
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log(errorThrown)
+  })
 }
 
-function fetchMap(makanId) {
-    pinLatitude = $('#'+makanId).find('.latitude').text()
-    pinLongitude = $('#'+makanId).find('.longitude').text()
+function fetchMap (makanId) {
+  pinLatitude = $('#' + makanId).find('.latitude').text()
+  pinLongitude = $('#' + makanId).find('.longitude').text()
 
-    console.log(pinLatitude)
-    console.log(pinLongitude)
+  console.log(pinLatitude)
+  console.log(pinLongitude)
 
-    $('#main').empty()
-    $('#indexText').empty()
-    window.location.href = './map.html?pinLat=' + pinLatitude + '&pinLng=' + pinLongitude + '&posLng=' + pos.lng + '&posLat=' + pos.lat
+  $('#main').empty()
+  $('#indexText').empty()
+  window.location.href = './map.html?pinLat=' + pinLatitude + '&pinLng=' + pinLongitude + '&posLng=' + pos.lng + '&posLat=' + pos.lat
 }
--- js/map.js
+++ js/map.js
@@ -1,11 +1,12 @@
-function getParameterByName(name, url) {
-   if (!url) url = window.location.href;
-   name = name.replace(/[\[\]]/g, "\\$&");
-   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-       results = regex.exec(url);
-   if (!results) return null;
-   if (!results[2]) return '';
-   return decodeURIComponent(results[2].replace(/\+/g, " "));
+/* global google */
+function getParameterByName (name, url) {
+  if (!url) url = window.location.href
+  name = name.replace(/[\[\]]/g, '\\$&')
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
 }
 
 var posLat
@@ -13,27 +14,27 @@ var posLng
 var pinLat
 var pinLng
 
-function initMap1() {
+function initMap1 () {
   posLat = +getParameterByName('posLat')
   posLng = +getParameterByName('posLng')
   pinLat = +getParameterByName('pinLat')
   pinLng = +getParameterByName('pinLng')
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer
+  var directionsService = new google.maps.DirectionsService
   var map = new google.maps.Map(document.getElementById('map1'), {
     zoom: 14,
     center: {lat: 37.77, lng: -122.447}
-  });
-  directionsDisplay.setMap(map);
+  })
+  directionsDisplay.setMap(map)
 
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
-  document.getElementById('mode').addEventListener('change', function() {
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-  });
+  calculateAndDisplayRoute(directionsService, directionsDisplay)
+  document.getElementById('mode').addEventListener('change', function () {
+    calculateAndDisplayRoute(directionsService, directionsDisplay)
+  })
 }
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-  var selectedMode = document.getElementById('mode').value;
+function calculateAndDisplayRoute (directionsService, directionsDisplay) {
+  var selectedMode = document.getElementById('mode').value
   directionsService.route({
     origin: {lat: posLat, lng: posLng},  // Haight.
     destination: {lat: pinLat, lng: pinLng},  // Ocean Beach.
@@ -41,11 +42,11 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
     // using square brackets and a string value as its
     // "property."
     travelMode: google.maps.TravelMode[selectedMode]
-  }, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
+  }, function (response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response)
     } else {
-      window.alert('Directions request failed due to ' + status);
+      window.alert('Directions request failed due to ' + status)
     }
-  });
+  })
 }
