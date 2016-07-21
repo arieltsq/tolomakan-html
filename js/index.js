93c08464b734d8649ad9095967683140eabc66dd
Ariel Tay <shihqian@gmail.com>
2016-07-21 09:57:45 +0800

    change to current location

--- js/index.js
+++ js/index.js
@@ -39,9 +39,8 @@ function fetchTolo () {
   $('#indexText').append('<h3> Here are the places you should makan today!</h3>')
   //  1.278668, 103.841098
   // 103.8434633, 1.2776249
-  // var data = 'lng=' + pos.lng + '&' + 'lat=' + pos.lat
-   var data = 'lng=' + 103.841098 + '&' + 'lat=' + 1.2776249
-  $.get('https://tolomakan.herokuapp.com/randomFive?' + data)
+  var data = 'lng=' + pos.lng + '&' + 'lat=' + pos.lat
+//  $.get('https://tolomakan.herokuapp.com/randomFive?' + data)
   .done(function (data) {
     data.forEach(function (datum) {
       // $('#main').append('<div class="col-xs-6 col-md-3"> <div class="thumbnail color"> <b>Place: </b>' + datum.name + '<br/>' + '<b>Address:</b> ' + datum.address + '<b>Categories:</b> ' + datum.categories + '</div></div>')
