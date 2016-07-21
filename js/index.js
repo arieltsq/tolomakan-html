06c1b974e4392ac8937c974708060c94f04e8ded
Ariel Tay <shihqian@gmail.com>
2016-07-21 18:51:28 +0800

    Added the apidoc

--- js/index.js
+++ js/index.js
@@ -36,7 +36,6 @@ function getBudget (price) {
   .done(function (data) {
     data.forEach(function (datum) {
       $('#main').append('<br><div id="'+data.indexOf(datum)+'" class="makanOption panel panel-default"><div class="panel-body"><div class="media-body"><p><h4 class="media-heading"> <div id="value" class=" bigger-font"><strong>Place: </strong>' + '<span class=datumName>' + datum.name + '</span>' + '</div></h4></p>' + '<p><b>Address:</b> ' + datum.address + '</p><p class="color"><b>Categories:</b> ' + datum.categories + '</p><p class= "type"><b>Type:</b> ' + datum.type + '<p class= "Price"><b>Price: $</b>' + datum.price + '<p class="latitude hide">' + datum.latitude + '</p><p class="longitude hide">' + datum.longitude + '</p></div></div></div>')
-
       $('#main').append('<div><button id="' + data.indexOf(datum) + 'Btn" class="btn-small saveOption"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span><p class="datumID hide">' + datum._id + '</p></button></div>')
     })
   }).fail(function (jqXHR, textStatus, errorThrown) {
